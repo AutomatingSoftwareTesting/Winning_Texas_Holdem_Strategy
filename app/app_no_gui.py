c74e320b7628f286074e868601042dcba105fc81
jdcald13 <jasonemailacct@gmail.com>
2017-09-17 14:14:03 -0600

    Refactor several variable/function names and enhanced feedback option

--- app/app_no_gui.py
+++ app/app_no_gui.py
@@ -19,16 +19,17 @@ class PlayGame(object):
     def get_hand_num(self):
         return int(self.hand_num)
 
-    def setup_game(self):
-        session_players = 6
-        range = "Unimproved Range.txt"
-        output_format = "csv"
-        feedback_file = app.feedback_file.FeedbackFile(session_players, output_format)
-        feedback_file.create_feedback_file()
-        show_feedback = True
-        return session_players, range, feedback_file, show_feedback
+    def setup_game(self, session_players=6, hand_range="Unimproved Range.txt", output_format="csv", show_feedback=False):
+        sp = session_players
+        hr = hand_range
+        of = output_format
+        ff = app.feedback_file.FeedbackFile(sp, of)
+        if show_feedback:  # Create feedback file only if user wants it. This will also allow non-Windows machines to play; just without feedback.
+            ff.create_feedback_file()
+        sf = show_feedback
+        return sp, hr, ff, sf
 
-    def play_hand(self, num_players, range, feedback_file, show_feedback):
+    def play_hand(self, num_players, hand_range, feedback_file, show_feedback):
         date_time = datetime.datetime.now().strftime("%m-%d-%y %H:%M:%S")
         print("Hand number: " + str(self.hand_num))
 
@@ -39,15 +40,15 @@ class PlayGame(object):
         hand.get_hand(deck)
         print(hand)
         hand.order_hand()
-        type = hand.hand_type()
+        hand_type = hand.hand_type()
 
         position = app.position.Position()
         position.current_position(num_players)
         print(position)
         position_min = position.min_open()
 
-        r = app.range.Range(range)
-        correct_decision, hand_percent, total_cards = r.correct_decision(type, position_min)
+        r = app.range.Range(hand_range)
+        correct_decision, hand_percent, total_cards = r.correct_decision(hand_type, position_min)
         # print(correct_decision)  Leaving for reference in case someone else isn't that familiar with hand ranges.
         # print(hand_percent)
         # print(total_cards)  # To confirm the hand percentage is correct - total_cards / 1326 = hand_percent
@@ -59,19 +60,23 @@ class PlayGame(object):
         decision = app.decision.Decision(action).decision()
 
         if decision != "stop":
-            if decision == correct_decision:
-                # screen feedback
-                print("Good job! You should", correct_decision, "because you want to play the top {0:.2f}".format(position_min * 100) + "% of your range; which ends at " + str(min_open_hand) + ".\n" + str(hand) +
-                      "is in the top {0:.2f}".format(hand_percent * 100) + "% of starting hands for the " + range + " range.")
-                feedback = "Correct"  # report feedback
-                self.score += 1
+            if show_feedback:
+                if decision == correct_decision:
+                    # screen feedback
+                    print("Good job! You should", correct_decision, "because you want to play the top {0:.2f}".format(position_min * 100) + "% of your range; which ends at " + str(min_open_hand) + ".\n" + str(hand) +
+                          "is in the top {0:.2f}".format(hand_percent * 100) + "% of starting hands for the " + hand_range + " range.")
+                    feedback = "Correct"  # report feedback
+                    self.score += 1
+                else:
+                    print("Sorry, you should", correct_decision, "because you want to play the top {0:.2f}".format(position_min * 100) + "% of your range; which ends at " + str(min_open_hand) + ".\n" + str(hand) +
+                          "is in the top {0:.2f}".format(hand_percent * 100) + "% of starting hands for the " + hand_range + " range.")
+                    feedback = "Incorrect"  # report feedback
+                feedback_file.save_hand(self.hand_num, date_time, hand_range, feedback, position, position_min, min_open_hand, hand, hand_type, hand_percent, decision, correct_decision, self.score)
+                self.hand_num += 1
+                print("Score: " + str(self.score) + "\n")
             else:
-                print("Sorry, you should", correct_decision, "because you want to play the top {0:.2f}".format(position_min * 100) + "% of your range; which ends at " + str(min_open_hand) + ".\n" + str(hand) +
-                      "is in the top {0:.2f}".format(hand_percent * 100) + "% of starting hands for the " + range + " range.")
-                feedback = "Incorrect"  # report feedback
-            feedback_file.save_hand(self.hand_num, date_time, range, feedback, position, position_min, min_open_hand, hand, type, hand_percent, decision, correct_decision, self.score)
-            self.hand_num += 1
-            print("Score: " + str(self.score) + "\n")
+                self.hand_num += 1
+                print()
         else:
             print("Thanks for playing.")
 
@@ -79,7 +84,7 @@ class PlayGame(object):
 
 
 start_game = PlayGame()
-num_players, range, file_extension, show_feedback = start_game.setup_game()
+session_num_players, session_range, session_file_extension, session_show_feedback = start_game.setup_game()
 play = None
 while play != 0:
-    play = start_game.play_hand(num_players, range, file_extension, show_feedback)
+    play = start_game.play_hand(session_num_players, session_range, session_file_extension, session_show_feedback)
